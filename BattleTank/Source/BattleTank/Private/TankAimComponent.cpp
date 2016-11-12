cf1e4d757c52e7676b21297c552f00ca284abf72
Maxpro <maxdietz@hotmail.com>
2016-11-12 01:51:00 -0800

    Fixed the bug for the AimSolution inside the TankAimComponent, refactored to use using declarations instead of #includes in headers.

--- BattleTank/Source/BattleTank/Private/TankAimComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimComponent.cpp
@@ -11,7 +11,7 @@ UTankAimComponent::UTankAimComponent()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	bWantsBeginPlay = true;
-	PrimaryComponentTick.bCanEverTick = true;
+	PrimaryComponentTick.bCanEverTick = true; // TODO: Should this really tick?
 
 	// ...
 }
@@ -27,19 +27,30 @@ void UTankAimComponent::AimAt(FVector HitLocation, float LaunchSpeed)
     
     FVector OutLaunchVelocity;
     FVector StartLocation = Barrel->GetSocketLocation(FName("Projectile"));
-    bool bHaveAimSolution = UGameplayStatics::SuggestProjectileVelocity // Calculate the OutLaunchVelocity
+    bool bHaveAimSolution = UGameplayStatics::SuggestProjectileVelocity
     (
         this,
         OutLaunchVelocity,
         StartLocation,
         HitLocation,
         LaunchSpeed,
-        ESuggestProjVelocityTraceOption::DoNotTrace
+        false,
+        0,
+        0,
+        ESuggestProjVelocityTraceOption::DoNotTrace // parameter must be present
     );
+    
     if (bHaveAimSolution)
     {
         auto AimDirection = OutLaunchVelocity.GetSafeNormal();
         MoveBarrelTowards(AimDirection);
+        auto Time = GetWorld()->GetTimeSeconds();
+        UE_LOG(LogTemp, Warning, TEXT("%f: Aim solution found"), Time);
+    }
+    else
+    {
+        auto Time = GetWorld()->GetTimeSeconds();
+        UE_LOG(LogTemp, Warning, TEXT("%f: No aim solve found %f"), Time);
     }
     // If no solution is found do nothing
 }
