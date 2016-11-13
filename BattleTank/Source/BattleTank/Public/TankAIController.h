04dfdcf46988a1d69f403db52d8711df33d9a83a
Maxpro <maxdietz@hotmail.com>
2016-11-13 03:58:52 -0800

    AI tanks are now setup to fire

--- BattleTank/Source/BattleTank/Public/TankAIController.h
+++ BattleTank/Source/BattleTank/Public/TankAIController.h
@@ -20,8 +20,5 @@ private:
     void BeginPlay() override;
     
     void Tick(float DeltaSeconds) override;
-    
-    ATank* GetControlledTank() const;
-    
-    ATank* GetPlayerTank() const;
+
 };
